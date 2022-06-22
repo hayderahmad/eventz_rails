9ba2d2bab54feb3dbdb3547d2b8917285235a9c2
hayderahmad@yahoo.com <hayderahmad@yahoo.com>
2022-06-21 23:26:57 -0600

    link pages and add update feature

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,16 @@ class EventsController < ApplicationController
     def index
         @events = Event.all
     end
+    def show
+        @event = Event.find(params[:id])
+    end
+    def edit
+        @event = Event.find(params[:id])
+    end
+    def update
+        @event = Event.find(params[:id])
+        event_params = params.require(:event).permit(:name, :description, :location, :price, :starts_at )
+        @event.update(event_params)
+        redirect_to event_path(@event)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root "events#index"
   get "events" => "events#index"
+  get "event/:id/edit" => "events#edit", as: "edit_event"
+  get "events/:id" => "events#show", as: "event"
+  patch "events/:id" => "events#update"
 end
