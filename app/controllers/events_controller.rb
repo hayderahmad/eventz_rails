e3830c696e29b148f862b5f8a9db751b33f9364e
hayderahmad@yahoo.com <hayderahmad@yahoo.com>
2022-08-19 19:04:19 -0600

    fix issues

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-    before_action :require_signin, except: [:index, :show]
+    before_action :require_signin
     before_action :require_admin, except: [:index, :show]
     def index
         case params[:filtered]
